b513b9e8762e8a11b78da7d1f7280a23fff5d6ab
FabioMorrone <morronefabio04@gmail.com>
2025-02-10 16:10:00 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+// numero di chilometri che vuole percorrere l'utente
+const kmPercorribili = Number(prompt( INSERIRE KM DA EFFETTUARE ES, 10,15,20 ECC.. )) 
+console.log(kmPercorribili);
+
+
+
+// età dell'utente
+
+
+
+// prezzo totale del viaggio
