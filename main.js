e29331bbe462753aeea02cdb53ad008232000256
FabioMorrone <morronefabio04@gmail.com>
2025-02-10 16:22:56 +0100

    add price

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 
 // numero di chilometri che vuole percorrere l'utente
-const kmPercorribili = Number(prompt( INSERIRE KM DA EFFETTUARE ES, 10,15,20 ECC.. )) 
+const kmPercorribili = Number(prompt("inserire il numero di km da percorrere , 10,15,20 ecc..")) 
 console.log(kmPercorribili);
 
 
 
 // età dell'utente
-
+const etàDelPasseggero = Number(prompt("inserire l'età del passeggero")); 
+console.log(etàDelPasseggero);
 
 
 // prezzo totale del viaggio
+
+const prezzoBiglietto = kmPercorribili * 0.21;
+console.log(prezzoBiglietto);
+
+
